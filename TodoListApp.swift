18839824a6affc5e60f2ab0270e8d8d2b7153358
Kobayashi-prac <df2jm2278f1p6@gmail.com>
2025-12-14 18:08:50 +0900

    first commit

---
+++ TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by swiftUI on 2025/12/14.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
