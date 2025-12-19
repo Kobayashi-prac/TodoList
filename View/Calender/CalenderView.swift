8628ba159d12ff031ddf240e2a467e7782ecb028
Kobayashi-prac <df2jm2278f1p6@gmail.com>
2025-12-20 05:14:09 +0900

    カレンダーをタップした時のは背景色を表すButtonStyleを追加

--- View/Calender/CalenderView.swift
+++ View/Calender/CalenderView.swift
@@ -44,7 +44,7 @@ struct CalenderView: View {
                                     Text("")
                                         .frame(width: 55, height: 100.0)
                                 }
-                                .background(backgroundColor)
+                                .buttonStyle(pressedButtonStyle())
 
                                 Text("\(date)")
                                 
@@ -59,29 +59,22 @@ struct CalenderView: View {
         }
     }
     
-    /// 背景塗りつぶしで角丸なボタンスタイル
-    struct RoundedButtonStyle: ButtonStyle {
-        @Environment(\.isEnabled) var isEnabled
+    /// ボタンが押された時のButtonStyle
+    struct pressedButtonStyle: ButtonStyle {
         
-        var color: Color = .blue
-        private let disabledColor: Color = .init(uiColor: .lightGray)
-        private let backgroundColor: Color = .white
-        private let cornerRadius: CGFloat = 8.0
-        private let lineWidth: CGFloat = 2.0
+        var backgroundColor: Color = .gray
+        var pressedBackgroundColor: Color = .white
         
         func makeBody(configuration: Configuration) -> some View {
             configuration.label
-                .padding()
-                .fontWeight(.bold)
-                .foregroundColor(.white)
-                // 有効無効でカラーを変更
-                .background(isEnabled ? color : disabledColor)
-                // 押下時かどうかで透明度を変更
-                .opacity(configuration.isPressed ? 0.5 : 1.0)
-                .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+                .background(backgroundColor(isPressed: configuration.isPressed))
         }
+        
+        func backgroundColor(isPressed: Bool) -> Color {
+            return isPressed ? backgroundColor : pressedBackgroundColor
+        }
+        
     }
-
 
 }
 
