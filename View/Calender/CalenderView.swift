41b8c65a983e07389d3a8ab0770a277490e530f2
Kobayashi-prac <df2jm2278f1p6@gmail.com>
2025-12-19 23:35:10 +0900

    カレンダーにタップ時の挙動を追加

--- View/Calender/CalenderView.swift
+++ View/Calender/CalenderView.swift
@@ -16,24 +16,69 @@ struct CalenderView: View {
     /// 1ヶ月
     let days = Array(1..<31)
     
+    @State var backgroundColor: Color = .white
+    @State var isPresented: Bool = false
+    
     var body: some View {
-        // alignmentGuideを使う
-        // layoutプロトコルを使う
-        // GeometryReaderとoffsetを使う
-        // LazyVGridを使う
-        VStack {
-            LazyVGrid(columns: columns, spacing: 0) {
-                ForEach(weekdays, id: \.self) { day in
-                    Text("\(day)")
-                }
-            }
-            ScrollView {
+
+        NavigationStack {
+            
+            VStack {
+                
+                // LazyVGridを使う
                 LazyVGrid(columns: columns, spacing: 0) {
-                    ForEach(days, id: \.self) { date in
-                        Text("\(date)")
+                    ForEach(weekdays, id: \.self) { day in
+                        Text("\(day)")
                     }
                 }
+                
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 3) {
+                        ForEach(days, id: \.self) { date in
+                            ZStack {
+                                
+                                Button {
+                                    backgroundColor = .gray
+//                                    isPresented = true
+                                } label: {
+                                    Text("")
+                                        .frame(width: 55, height: 100.0)
+                                }
+                                .background(backgroundColor)
+
+                                Text("\(date)")
+                                
+                            }
+                        }
+                    }
+                }
+                .fullScreenCover(isPresented: $isPresented) {
+                    EmptyView()
+                }
             }
+        }
+    }
+    
+    /// 背景塗りつぶしで角丸なボタンスタイル
+    struct RoundedButtonStyle: ButtonStyle {
+        @Environment(\.isEnabled) var isEnabled
+        
+        var color: Color = .blue
+        private let disabledColor: Color = .init(uiColor: .lightGray)
+        private let backgroundColor: Color = .white
+        private let cornerRadius: CGFloat = 8.0
+        private let lineWidth: CGFloat = 2.0
+        
+        func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .padding()
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                // 有効無効でカラーを変更
+                .background(isEnabled ? color : disabledColor)
+                // 押下時かどうかで透明度を変更
+                .opacity(configuration.isPressed ? 0.5 : 1.0)
+                .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
         }
     }
 
