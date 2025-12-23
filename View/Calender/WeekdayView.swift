105eb6b0326dc16702aa0ab92c1d36e30596c030
Kobayashi-prac <df2jm2278f1p6@gmail.com>
2025-12-23 22:29:36 +0900

    専用ファイルを作成

--- View/Calender/WeekdayView.swift
+++ View/Calender/WeekdayView.swift
@@ -7,9 +7,20 @@
 
 import SwiftUI
 
+/// 一週間の日付配列
+let columns = Array(repeating: GridItem(.flexible()), count: 7)
+
+/// 曜日
+let weekdays = ["日", "月", "火", "水", "木", "金", "土"]
+
 struct WeekdayView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // LazyVGridを使う
+        LazyVGrid(columns: columns, spacing: 0) {
+            ForEach(weekdays, id: \.self) { day in
+                Text("\(day)")
+            }
+        }
     }
 }
 
