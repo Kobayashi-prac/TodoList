85ecc1c2d018daafb9e0fbc76da6cc8d46d9d35a
Kobayashi-prac <df2jm2278f1p6@gmail.com>
2025-12-23 22:28:49 +0900

    専用ファイルを作成

--- View/Calender/MonthsView.swift
+++ View/Calender/MonthsView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct MonthsView: View {
+    
+    /// 一週間の日付配列
+    let columns = Array(repeating: GridItem(.flexible()), count: 7)
+
+    /// 1ヶ月
+    let days = Array(1..<31)
+
+    @Binding var isVisibleTodoList: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 3) {
+                ForEach(days, id: \.self) { date in
+                    ZStack {
+                        // isPressedの通知を受け取るのは押されたボタンだけ
+                        Button {
+                            withAnimation {
+                                isVisibleTodoList = true
+                            }
+                        } label: {
+                            // この引数はViewに準拠していればなんでも返して良い
+                            Text("")
+                                .frame(width: 55, height: 50.0)
+                        }
+                        .buttonStyle(pressedButtonStyle())
+                        
+                        Text("\(date)")
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    MonthsView()
+    MonthsView(isVisibleTodoList: .constant(false))
 }
